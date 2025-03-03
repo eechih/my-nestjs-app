80c427ec81398a7a30b3de786fbb59b9c00699a0
Harry Yeh <eechih@gmail.com>
2025-03-03 10:01:37 +0800

    Integrate S3 Client to upload invoice files to S3 bucket

--- src/invoices/invoices.controller.ts
+++ src/invoices/invoices.controller.ts
@@ -1,6 +1,9 @@
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { InjectQueue } from '@nestjs/bull';
 import { Controller, Post } from '@nestjs/common';
 import { Queue } from 'bull';
+
+const s3Client = new S3Client({ region: 'us-east-1' });
 
 @Controller('invoices')
 export class InvoicesController {
@@ -11,5 +14,21 @@ export class InvoicesController {
     await this.invoicesQueue.add('crawl', {
       file: 'audio.mp3',
     });
+  }
+
+  @Post('upload')
+  async upload() {
+    console.log('Uploading invoice to ASW S3');
+    const command = new PutObjectCommand({
+      Bucket: 'amplify-drinkshopbackend-e-userfilesbucketafbcc364-pjj2hahj0zoo',
+      Key: `upload/invoice_${new Date().getTime()}.json`,
+      Body: JSON.stringify({
+        name: 'test',
+        createdAt: new Date().toISOString(),
+      }),
+    });
+
+    const res = await s3Client.send(command);
+    return res;
   }
 }
